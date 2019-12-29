ce962e9d0fb8185c68681f38b0671dc0fbafeb8f
Douglas Johnson <douglas.j.johnson@gmail.com>
2019-12-29 00:20:07 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,36 @@
-# My Code here....
+def map_to_negativize (source_array)
+  return_array = []
+
+  return return_array
+end
+
+def map_to_no_change (source_array)
+  return_array = []
+
+  return return_arrayend
+
+def map_to_double (source_array)
+  return_array = []
+
+  return return_arrayend
+
+def map_to_square (source_array)
+  return_array = []
+
+  return return_arrayend
+
+def reduce_to_total (source_array, starting_point)
+  return_array = []
+
+  return return_arrayend
+
+def reduce_to_all_true (source_array)
+  return_array = []
+
+  return return_arrayend
+
+def reduce_to_any_true (source_array)
+  return_array = []
+
+  return return_array
+end
