debff44508942298073d6a5faf8d01386b699079
Douglas Johnson <douglas.j.johnson@gmail.com>
2019-12-29 00:20:45 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -7,27 +7,32 @@ end
 def map_to_no_change (source_array)
   return_array = []
 
-  return return_arrayend
+  return return_array
+end
 
 def map_to_double (source_array)
   return_array = []
 
-  return return_arrayend
+  return return_array
+end
 
 def map_to_square (source_array)
   return_array = []
 
-  return return_arrayend
+  return return_array
+end
 
 def reduce_to_total (source_array, starting_point)
   return_array = []
 
-  return return_arrayend
+  return return_array
+end
 
 def reduce_to_all_true (source_array)
   return_array = []
 
-  return return_arrayend
+  return return_array
+end
 
 def reduce_to_any_true (source_array)
   return_array = []
