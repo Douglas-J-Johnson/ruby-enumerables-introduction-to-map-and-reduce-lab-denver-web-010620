3267044d326c2e84326f8a2cc84a09a55f9f2c4a
Douglas Johnson <douglas.j.johnson@gmail.com>
2019-12-29 00:25:58 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,5 +1,12 @@
+#MAP - return an ARRAY
 def map_to_negativize (source_array)
   return_array = []
+
+  if source_array
+    for i in 0...source_array.length
+
+    end
+  end
 
   return return_array
 end
@@ -7,11 +14,23 @@ end
 def map_to_no_change (source_array)
   return_array = []
 
+  if source_array
+    for i in 0...source_array.length
+
+    end
+  end
+
   return return_array
 end
 
 def map_to_double (source_array)
   return_array = []
+
+  if source_array
+    for i in 0...source_array.length
+
+    end
+  end
 
   return return_array
 end
@@ -19,19 +38,44 @@ end
 def map_to_square (source_array)
   return_array = []
 
+  if source_array
+    for i in 0...source_array.length
+
+    end
+  end
+
   return return_array
 end
 
+#REDUCE - return a SCALAR
 def reduce_to_total (source_array, starting_point)
   total = 0
+
+  if source_array
+    for i in 0...source_array.length
+
+    end
+  end
 
   return total
 end
 
 def reduce_to_all_true (source_array)
 
+  if source_array
+    for i in 0...source_array.length
+
+    end
+  end
+
 end
 
 def reduce_to_any_true (source_array)
+
+  if source_array
+    for i in 0...source_array.length
+
+    end
+  end
 
 end
